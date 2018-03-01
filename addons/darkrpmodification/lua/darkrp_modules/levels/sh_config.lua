46a6b40bff4057ac4e6977e123948e502a329736
septosezero <35184029+septosezero@users.noreply.github.com>
2018-03-01 02:59:41 -0500

    Update sh_config.lua

--- addons/darkrpmodification/lua/darkrp_modules/levels/sh_config.lua
+++ addons/darkrpmodification/lua/darkrp_modules/levels/sh_config.lua
@@ -25,9 +25,9 @@ LevelSystemConfiguration.NPCXP = true // Give XP when an NPC is killed?
 LevelSystemConfiguration.NPCXPAmount = 10 // Amount of XP to give when an NPC is killed
 
 LevelSystemConfiguration.TimerModule = true // Give XP to everybody every howeverlong
-LevelSystemConfiguration.Timertime = 100 // How much time (in seconds) until everybody gets given XP
+LevelSystemConfiguration.Timertime = 900 // How much time (in seconds) until everybody gets given XP
 LevelSystemConfiguration.TimerXPAmount = 100 // How much XP to give each time it goes off
-LevelSystemConfiguration.YourServerName = "on the server" // The notifcation text ish. "You got 100XP for playing on the server."
+LevelSystemConfiguration.YourServerName = "on the server" // The notifcation text ish. "You received 900 XP for playing on the server."
 
 LevelSystemConfiguration.XPMult = 1 // How hard it is to level up. 2 would require twice as much XP, ect.
 LevelSystemConfiguration.MaxLevel = 99 // The max level
